bb514e4ff2eca704401e5cbbe308d3464edee050
Glenna Mowry & Nicole <pair+gmmowry.nicole-a-tesla@devbootcamp.com>
2015-02-10 21:57:02 -0600

    took out coffeescript, created 3 vector objects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,3 @@
-// new Raphael(element, width, height)
-
-window.onload = function() {
-  var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
-
-  var circle = paper.circle(100, 100, 80);
-
-  for(var i = 0; i < 20; i+=1) {
-      var multiplier = i*5;
-      paper.circle(250 + (2*multiplier), 100 + multiplier, 50 - multiplier)
-  }
-
-  var rectangle = paper.rect(200, 200, 250, 100);
-
-  var ellipse = paper.ellipse(200, 400, 100, 50);
-
-  paper.path("M 250 250")
-
-};
-
-
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -35,6 +13,83 @@ window.onload = function() {
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+//= require raphael.js
+
+
+
+window.onload = function() {
+  var paper = new Raphael(document.getElementById('canvas_container'), 800, 800);
+
+  // paper.attr({fill: 'grey', stroke: '#ddd', 'stroke-width': 2});
+
+  var circle = paper.circle(100, 100, 80);
+
+  circle.mouseover(function(){
+    console.log("NOPE!");
+    circle.attr("fill", "red")
+  });
+
+  circle.mouseout(function(){
+    console.log("YEP!");
+    circle.attr("fill", "white");
+  });
+
+  var rectangle = paper.rect(200, 200, 250, 100);
+
+  rectangle.attr("fill", "grey");
+
+  rectangle.glow({width: 10, fill: false, opacity: 0.5, offsetx: 0, offsety: 0, color: '#208120'});
+
+  // var ellipse = paper.ellipse(200, 400, 100, 50);
+
+// creating a prototype for a set of objects to call them 'draggable'
+// setting variables we'll need let coordinates, moving functionality, etc.
+// set the reference 'me' because Raphael will only register the drag event of one element, but we want the whole set moving.
+// (lx, ly) are their current location, (ox, oy) were the start location
+// (dx, dy) are the differences in the two vector points
+// endFnc then resets the start location as the current location for the next time you drag
+
+  Raphael.st.draggable = function() {
+    var me = this
+      lx = 0,
+      ly = 0,
+      ox = 0,
+      oy = 0,
+      moveFnc = function(dx, dy) {
+        lx = dx + ox;
+        ly = dy + oy;
+        me.transform('t' + lx + ',' + ly);
+      },
+      startFnc = function() {},
+      endFnc = function() {
+        ox = lx;
+        oy = ly;
+      };
+
+    this.drag(moveFnc, startFnc, endFnc);
+  };
+
+  var mySet = paper.set();
+
+  mySet.push(paper.circle(350, 450, 50).attr('fill', 'blue'));
+  mySet.push(paper.circle(350, 450, 40).attr('fill', 'white'));
+  mySet.push(paper.circle(350, 450, 30).attr('fill', 'blue'));
+  mySet.push(paper.circle(350, 450, 20).attr('fill', 'white'));
+  mySet.push(paper.circle(350, 450, 10).attr('fill', 'blue'));
+
+  mySet.draggable();
+};
+
+
+
+  //
+
+  // var newshape = paper.path("M 350 350 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
+  // newshape.attr({fill: 'grey', stroke: '#ddd', 'stroke-width': 5, rotation: -90});
+
+  // newshape.animate(function(){ {rotation: 360}}, 2000, 'bounce');
+
+//click on rectangle, make it bigger
 
 

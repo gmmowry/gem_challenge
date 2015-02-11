d6bf4b6235c3fcfd62f03d95c3eee02109c308ed
nicole-a-tesla <thisisnotnicole@gmail.com>
2015-02-10 18:41:47 -0600

    jaaaaaaaavascriiiiiiiiipt and html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,25 @@
+// new Raphael(element, width, height)
+
+window.onload = function() {
+  var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
+
+  var circle = paper.circle(100, 100, 80);
+
+  for(var i = 0; i < 20; i+=1) {
+      var multiplier = i*5;
+      paper.circle(250 + (2*multiplier), 100 + multiplier, 50 - multiplier)
+  }
+
+  var rectangle = paper.rect(200, 200, 250, 100);
+
+  var ellipse = paper.ellipse(200, 400, 100, 50);
+
+  paper.path("M 250 250")
+
+};
+
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
